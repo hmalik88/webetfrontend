cc14dda07063f83b1474136d6ff2d2ec1e4f0970
hmalik88 <hbmalik88@gmail.com>
2019-05-20 17:40:27 -0400

    added in contract card for marketplace

--- src/components/ContractCard.js
+++ src/components/ContractCard.js
@@ -4,16 +4,18 @@ import ContractPic from '../assets/contract.svg'
 
 export default class ContractCard extends React.Component {
 
+
+
   render() {
     return(
-      <div className="contract-card">
+      <div className="contract-card" onClick={this.handleClick}>
         <div className="image-section">
           <img src={ContractPic} className="contract-pic" alt="" />
+          <div className="eth-price">0.15<br/>ETH</div>
         </div>
-        <div className="eth-price">0.15<br/>ETH</div>
         <div className="card-info">
-          <div className="bet"></div>
-          <div className="exp-date"></div>
+          <div className="bet">ETH > $300.00</div>
+          <div className="exp-date">EXP: 5/25/19</div>
         </div>
       </div>
       )
